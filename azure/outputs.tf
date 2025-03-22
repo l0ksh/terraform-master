38618554f437b82749ffb5259493636d8e428652
l0ksh <lokesh904@gmail.com>
2025-03-22 20:30:17 +0000

    update outputs.tf

--- azure/outputs.tf
+++ azure/outputs.tf
@@ -1,4 +1,5 @@
 # outputs.tf
 output "public_ip_address" {
-  value = azurerm_public_ip.public_ip.ip_address
+  description = "The public IP address of the virtual machine"
+  value       = azurerm_public_ip.public_ip.ip_address
 }
